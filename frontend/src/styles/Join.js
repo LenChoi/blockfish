0259221796ea942609e7cebbf378db6a15656f77
MBC <mbc0481@naver.com>
2021-05-09 20:17:12 +0900

    회원가입 페이지 개발

---
+++ frontend/src/styles/Join.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const JoinContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 80px 50px 30px 50px;
+`;
+export const JoinCheckboxWrapper = styled.div`
+  margin: 60px 0 30px 0;
+`;
+export const JoinContentWrapper = styled.div`
+  max-width: 250px;
+`;
