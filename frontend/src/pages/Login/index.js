33674088f1fda2ae65d9e11b3535f871dfeb5c1d
rimseo12 <seorimhyun@gmail.com>
2021-05-10 16:25:22 +0900

    로그인 화면 UI 구현
    - 사용자 아이디(이메일), 비밀번호 항목 추가됨

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import { InputAdornment, TextField } from '@material-ui/core';
+import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import DefaultLayout from '../../layouts/DefaultLayout';
+import TextDefault from '../../components/ui/TextDefault';
+import { LoginContainer, LoginWrapper } from '../../styles/Login';
 
-const Login = () => <div>Login 페이지 입니다.</div>;
+const Login = () => {
+  const loginText = '로그인';
+  return (
+    <DefaultLayout>
+      <LoginContainer>
+        <TextDefault size="30px" weight="700">
+          {loginText}
+        </TextDefault>
+        <LoginWrapper>
+          <TextField
+            label="아이디(이메일)"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PermIdentityOutlinedIcon />
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginTop: '10px' }}
+          />
+          <TextField
+            label="비밀번호"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlinedIcon />
+                </InputAdornment>
+              ),
+            }}
+            style={{ marginTop: '10px' }}
+          />
+        </LoginWrapper>
+      </LoginContainer>
+    </DefaultLayout>
+  );
+};
 
 Login.propTypes = {};
 
---
+++ frontend/src/styles/Login.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const LoginContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const LoginWrapper = styled.div`
+  margin-left: 10px;
+  display: inline-grid;
+`;
