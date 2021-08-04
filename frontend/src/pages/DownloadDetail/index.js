0bbef7dccb6a2fc9cec98c94cf2b8a991e6a1106
MBC <mbc0481@naver.com>
2021-08-04 22:34:20 +0900

    download API 테스트

--- frontend/src/pages/DownloadDetail/index.js
+++ frontend/src/pages/DownloadDetail/index.js
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { postApi } from '../../api/postApi';
 import BreadScrumb from '../../components/downloadDetail/BreadScrumb';
 import DownloadDetailLeft from '../../components/downloadDetail/DownloadDetailLeft';
 import DownloadDetailRight from '../../components/downloadDetail/DownloadDetailRight';
 import { DnldDetailWrapper } from '../../styles/DownloadDetail';
+import { isEmpty } from '../../utils/utils';
 
 const DownloadDetail = (props) => {
   const { match } = props;
+  const [downloadId, setDownloadId] = useState(match.params.id);
 
   useEffect(() => {
     // API 호출
     console.log('match.params', match.params.id);
+
+    const data = postApi('/file/downloadTest', {
+      fileName: 'top_search.png',
+    });
+
+    try {
+      if (!isEmpty(data) && data.status === 200) {
+        console.log(data);
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   return (
